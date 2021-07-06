683e257a9facedcab1790f136d467dfab0e066a4
SrGitQ <jmbalanzar@gmail.com>
2021-07-06 09:23:16 -0500

    aplication structured

--- ios/FoodAppACTests/FoodAppACTests.m
+++ ios/FoodAppACTests/FoodAppACTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FoodAppTests : XCTestCase
+@interface FoodAppACTests : XCTestCase
 
 @end
 
-@implementation FoodAppTests
+@implementation FoodAppACTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

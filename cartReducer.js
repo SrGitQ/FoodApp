ea3da6b8c725038a3d37d224ff0e4e662c7c2b20
SrGitQ <jmbalanzar@gmail.com>
2021-07-10 23:49:43 -0500

    reducers down

--- cartReducer.js
+++ cartReducer.js
@@ -1,16 +1,28 @@
 import { combineReducers } from "redux";
 
 const INITIAL_STATE = {
-    current:[]
+    current:"Hola carnal"
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
+        case 'ADD_ITEM':
+            const {
+                current
+            } = state
+            const addedItem = action.payload
+
+            current.push(addedItem)
+
+            const newState = { current }
+
+            return newState
+
         default:
             return state
     }
 }
 
 export default combineReducers({
-    cart: cartReducer
+    items: cartReducer
 })
